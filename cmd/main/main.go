63dc30a10b46f0c53b647e64a9add4b7f10b09e9
stewthestew <stew156@proton.me>
2025-05-12 22:03:55 +0000

    feat: Will no longer be blocking by default, instead asks user

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -54,13 +54,35 @@ func main() {
 	if len(os.Getenv("DEBUG")) > 0 {
 		log.Debug("selected", selectedCmd)
 	}
+
 	executedCmd := exec.Command(selectedCmd)
 	executedCmd.Stdout = os.Stdout
 	executedCmd.Stdin = os.Stdin
 	executedCmd.Stderr = os.Stderr
+	// .RUN() Is blocking which is good for tui apps, but for gui's? No.
+	var choice string
+	fmt.Println("Do you want to run this in the current window? [y\\N]")
+	// TODO: Use a buffer reader instead (Most likely will never do)
+	_, err = fmt.Scanln(&choice)
+	if err != nil {
+		log.Error("Failed to read input", err)
+		err = executedCmd.Run()
+	}
 
-	err = executedCmd.Run()
+	switch strings.ToLower(choice) {
+	case "y":
+		err = executedCmd.Run()
+	case "yes":
+		err = executedCmd.Run()
+	case "n":
+		err = executedCmd.Start()
+	case "no":
+		err = executedCmd.Start()
+	default:
+		log.Info("Choosing default")
+		err = executedCmd.Run()
 
+	}
 	if err != nil {
 		if len(os.Getenv("IMPORTANT_DEBUG")) > 0 {
 			log.Fatal(err)
