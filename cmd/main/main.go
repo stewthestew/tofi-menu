dea7c8fa626ba22c4e4ea919881a7892b0131084
stewthestew <stew156@proton.me>
2025-05-12 19:25:33 +0000

    fix: no longer executes every command in $PATH

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -16,12 +16,14 @@ func main() {
 
 	a := strings.Join(list, "\n")
 
-	cmd := exec.Command("/bin/sh", "-c", a)
+	cmd := exec.Command("fzf")
 	cmd.Stdout = os.Stdout
 	cmd.Stdin = strings.NewReader(a)
 	cmd.Stderr = os.Stderr
 	err = cmd.Run()
 	if err != nil {
-		log.Fatal(err)
+		if len(os.Getenv("DEBUG")) > 0 {
+			log.Fatal(err)
+		}
 	}
 }
