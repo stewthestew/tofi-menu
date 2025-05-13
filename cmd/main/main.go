a316b6799398b381c44191e199a82b1e886f9fd3
stewthestew <stew156@proton.me>
2025-05-13 18:48:26 +0000

    refactor: Better ux & better user input management

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"os/exec"
@@ -12,6 +13,7 @@ import (
 )
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
 	list, err := backend.List(os.Getenv("PATH"))
 	fzf := os.Getenv("FZF_PATH")
 
@@ -59,24 +61,20 @@ func main() {
 
 	executedCmd := exec.Command(selectedCmd)
 	// .RUN() Is blocking which is good for tui apps, but for gui's? No.
+	fmt.Print("Do you want to run this in the current window? [yes\\no\\Quit]\n: ")
+
 	var choice string
-	fmt.Println("Do you want to run this in the current window? [y\\N]")
-	_, err = fmt.Scanln(&choice)
+	choice, err = reader.ReadString('\n')
 	if err != nil {
 		log.Error(err)
 	}
 
-	if strings.ToLower(choice) == "" || strings.ToLower(choice) == "\n" {
-		choice = "n"
-	}
+	choice = strings.TrimSpace(choice)
 
 	switch strings.ToLower(choice) {
-	case "y", "yes":
-		executedCmd.Stdout = os.Stdout
-		executedCmd.Stdin = os.Stdin
-		executedCmd.Stderr = os.Stderr
+	case "q", "quit":
+		log.Info("Bye!")
 
-		err = executedCmd.Run()
 	case "n", "no":
 		executedCmd.Stdout = nil
 		executedCmd.Stderr = nil
@@ -84,13 +82,16 @@ func main() {
 		executedCmd.SysProcAttr = &syscall.SysProcAttr{
 			Setsid: true,
 		}
-
 		err = executedCmd.Start()
 	default:
-		log.Info("Choosing default")
+		executedCmd.Stdout = os.Stdout
+		executedCmd.Stdin = os.Stdin
+		executedCmd.Stderr = os.Stderr
+
 		err = executedCmd.Run()
 
 	}
+
 	if err != nil {
 		if len(os.Getenv("IMPORTANT_DEBUG")) > 0 {
 			log.Fatal(err)
